4b8848f7ab62e05be6697d7e6e31fdce9eb63147
halo054 <halo63604@gmail.com>
2024-02-25 09:20:18 +0800

    Add "set_the_hook"/enable restart

---
+++ Fishing_Shooting/Assets/scripts/change_background_music.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_background_music : MonoBehaviour
+{
+    public static change_background_music instance;
+
+    public AudioClip initialMusic;
+    public AudioClip secondMusic;
+
+    private AudioSource audioSource;
+    private bool _set_hook_flag;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = initialMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+           
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        audioSource.clip = initialMusic;
+        if (audioSource.isPlaying == false)
+        {
+            audioSource.Play();
+        }
+        
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "2.24 Yuan's version")
+        {
+            // 获取另一个物体上的 FishController 脚本，并获取其中的 _set_hook_flag 变量
+            fish_moving_WASD_uprightdownleft fishController = FindObjectOfType<fish_moving_WASD_uprightdownleft >();
+            if (fishController != null)
+            {
+                _set_hook_flag = fishController._set_hook_flag;
+                // 每帧检查 _fish_on_flag 的值，根据其值选择要播放的音乐
+                
+            }
+           
+            if (_set_hook_flag)
+            {
+                if (audioSource.clip != secondMusic)
+                {
+                    audioSource.clip = secondMusic;
+                    audioSource.Play();
+                }
+            }
+            else
+            {
+                if (audioSource.clip != initialMusic)
+                {
+                    audioSource.clip = initialMusic;
+                    audioSource.Play();
+                }
+            }
+        }
+        else
+        {
+            audioSource.clip = initialMusic;
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
---
+++ Fishing_Shooting/Assets/scripts/choose_level_scene_manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class choose_level_scene_manager : MonoBehaviour
+{
+    public void choosing_level(string sceneName)
+    {
+        Debug.Log("Accessed");
+        SceneManager.LoadScene(sceneName);
+    }
+}
